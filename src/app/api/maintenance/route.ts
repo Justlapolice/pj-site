234979a42cd9eaf1880308bf99b23d8cb70d58f9
JustForever974 <treunion97@gmail.com>
2025-08-04 22:20:57 +0200

    maintenance off

--- src/app/api/maintenance/route.ts
+++ src/app/api/maintenance/route.ts
@@ -3,6 +3,6 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   // Par défaut, le mode maintenance est désactivé par défaut
   // Pour l'activer, mettre true, et pour le desactiver mettre false
-  const isMaintenance = true;
+  const isMaintenance = false;
   return NextResponse.json({ isMaintenance });
 }
