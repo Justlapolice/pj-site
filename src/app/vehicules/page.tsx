ad46958dc6ab157108977730b654f205b0d4585a
Justlapolice <Policejudiciairefr75@outlook.fr>
2025-09-02 19:51:12 +0200

    Ajout de la nouvelle fonctionnalité

--- src/app/vehicules/page.tsx
+++ src/app/vehicules/page.tsx
@@ -194,7 +194,7 @@ function VehiculesCRS() {
         <div className="animate-pulse flex flex-col items-center">
           <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
           <p className="mt-4 text-gray-300">
-            Chargement de la page {pathname} en cours...
+            Chargement de la page en cours...
           </p>
         </div>
       </div>
